f3ebb0ea4d5d1822be4fbbc83b3a83649f335245
drhoneyroll <orlicaleksandar97@gmail.com>
2026-03-14 21:06:53 +0100

    update

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Object that entered the trigger: " + other);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+
 
 public class PlayerController : MonoBehaviour
+
 {
+    Rigidbody2D rigidbody2d;
+    public int maxHealth = 5;
+    int currentHealth;
+    Vector2 move;
+    public float movement_speed;
+    public InputAction MoveAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate=10;
+        currentHealth = maxHealth;
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
     }
+    void FixedUpdate()
+    {
+    Vector2 position = (Vector2)rigidbody2d.position + move * movement_speed * Time.deltaTime;
+    //Vector2 position = (Vector2)rigidbody2d.position + move * 5.0f * Time.deltaTime;
+    rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
