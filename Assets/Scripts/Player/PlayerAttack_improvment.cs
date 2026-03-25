ea7bced0f27673d5ae6288828975fbf25c8b796b
drhoneyroll <orlicaleksandar97@gmail.com>
2026-03-25 01:01:15 +0100

    BIG UPDATE - Player Movement and Attack

---
+++ Assets/Scripts/Player/PlayerAttack_improvment.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack_improvment : MonoBehaviour
+{
+    public Animator anim;
+    [SerializeField] private Transform attackTransform;
+    [SerializeField] private float attackRange = 1.5f;
+    [SerializeField] private LayerMask attackableLayer;
+    [SerializeField] private float damageAmount = 1f;
+    [SerializeField] InputAction inputAction;
+
+    private RaycastHit2D[] hits;
+
+    //private Animator anim;
+
+    [SerializeField] private float timeBtwAttacks = 0.15f;
+    public bool ShouldBeDamaging {get; private set; } = false;
+    private float attackTimeCounter;
+
+    private List<IDamageable> iDamageables = new List<IDamageable>();
+
+    void Awake()
+    {
+        inputAction.Enable();
+    }
+
+    private void Start()
+    {
+        anim=GetComponent<Animator>();
+        attackTimeCounter = timeBtwAttacks;
+    }
+
+    private void Update()
+    {
+        if (inputAction.WasPressedThisFrame() && attackTimeCounter >= timeBtwAttacks)
+        {
+            Attack();
+            //anim.SetTrigger("punch");
+            attackTimeCounter = 0f;
+        }
+
+        attackTimeCounter += Time.deltaTime;
+    }
+
+    public void Attack()
+    {
+        anim.SetBool("IsAttacking",true);      
+    }
+
+    public void FinishAttacking()
+    {
+        anim.SetBool("IsAttacking",false);
+    }
+
+    public IEnumerator DamageWhileSlashIsActive()
+    {
+        ShouldBeDamaging = true;
+        //Debug.Log("Im in DamageWhileSlashIsActive");
+        while(ShouldBeDamaging)
+        {
+            Debug.Log("Im in while loop");
+            hits = Physics2D.CircleCastAll(attackTransform.position, attackRange, transform.right, 0f, attackableLayer);
+
+            for (int i = 0; i < hits.Length; i++) //vrti kroz sve targete zahvacene CircleCastom
+            {
+                Debug.Log("Iteracija for petlje"+i+' '+hits.Length);
+                IDamageable iDamageable = hits[i].collider.gameObject.GetComponent<IDamageable>();
+                
+
+                //iDamageable.Damage(damageAmount);
+                //iDamageables.Add(iDamageable);
+                //Debug.Log("iDamageable: "+iDamageable+"   iDamageable.HasTakenDamage: "+iDamageable.HasTakenDamage);
+                if (iDamageable != null && !iDamageable.HasTakenDamage)
+                {
+                    Debug.Log("Damage?");
+                    iDamageable.Damage(damageAmount);
+                    iDamageables.Add(iDamageable);
+                }
+            }
+            Debug.Log("Should Be Damaging: "+ ShouldBeDamaging);
+            yield return null; //wait for one more frame (game will freeze without this)
+        }
+
+        ReturnAttackablesToDamageable();
+    }
+
+    private void ReturnAttackablesToDamageable()
+    {
+        Debug.Log("ReturnAttackablesToDamageable");
+        foreach (IDamageable thingThatWasDamaged in iDamageables)
+        {
+            Debug.Log("thingThatWasDamaged: " + thingThatWasDamaged + " " + thingThatWasDamaged.HasTakenDamage);
+            thingThatWasDamaged.HasTakenDamage = false;
+            Debug.Log("thingThatWasDamaged: " + thingThatWasDamaged + " " + thingThatWasDamaged.HasTakenDamage);
+        }
+
+        iDamageables.Clear();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackTransform.position,attackRange);
+    }
+
+    #region Animation Triggers
+
+    public void ShouldBeDamagingToTrue()
+    {
+        ShouldBeDamaging = true;
+    }
+
+    public void ShouldBeDamagingToFalse()
+    {
+        ShouldBeDamaging = false;
+    }
+
+    #endregion
+
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,10 +13,20 @@ public class PlayerController : MonoBehaviour
     Vector2 move;
     public float movement_speed;
     public InputAction MoveAction;
-    Animator animator;
-    Vector2 moveDirection = new Vector2(1, 0);
+    public Animator animator;
+
+    [SerializeField] public InputAction AttackAction;
+
+    public PlayerAttack_improvment player_Combat;
+
+
+    Vector2 moveDirection = new Vector2(0, 0);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    void Awake()
+    {
+        AttackAction.Enable();
+    }
     void Start()
     {
         //QualitySettings.vSyncCount = 0;
@@ -24,37 +35,84 @@ public class PlayerController : MonoBehaviour
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
+        animator.SetFloat("horizontal", 0f);
+        animator.SetFloat("vertical", 0f);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
+        
+
+
+        if (AttackAction.WasPressedThisFrame())
+        {
+            player_Combat.Attack();
+        }
+        //Debug.Log(move);
+
+        if (move.magnitude <= 0.1)
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+    }
+    void FixedUpdate()
+    {
+        /*
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
+        */
+
         move = MoveAction.ReadValue<Vector2>();
+        //Debug.Log("move "+move);
         if(!Mathf.Approximately(move.x,0.0f) || !Mathf.Approximately(move.y,0.0f))
         {
             moveDirection.Set(move.x, move.y);
             moveDirection.Normalize();
         }
-        animator.SetFloat("Look X", moveDirection.x);
-        animator.SetFloat("Look Y", moveDirection.y);
+        else
+        {
+            moveDirection.Set(0f,0f);
+        }
+
+        if (moveDirection.x > 0 && transform.localScale.x < 0 ||
+                moveDirection.x < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        animator.SetFloat("horizontal", Mathf.Abs(moveDirection.x));
+        animator.SetFloat("vertical", Mathf.Abs(moveDirection.y));
+        //animator.SetFloat("horizontal", moveDirection.x);
+        //animator.SetFloat("vertical", moveDirection.y);
         animator.SetFloat("Speed", move.magnitude);
 
-        //Debug.Log(move);
+        Vector2 position = (Vector2)rigidbody2d.position + move * movement_speed * Time.deltaTime;
+
+        //Vector2 position = (Vector2)rigidbody2d.position + move * 5.0f * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+        //animator.SetFloat("horizontal", 0f);
+        //animator.SetFloat("vertical", 0f);
     }
-    void FixedUpdate()
+    
+    void Flip()
     {
-    Vector2 position = (Vector2)rigidbody2d.position + move * movement_speed * Time.deltaTime;
-    //Vector2 position = (Vector2)rigidbody2d.position + move * 5.0f * Time.deltaTime;
-    rigidbody2d.MovePosition(position);
+        //facingDirection *= -1;
+        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
     public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         //currentHealth+=amount;
-        Debug.Log(currentHealth + "/" + maxHealth);
+        //Debug.Log(currentHealth + "/" + maxHealth);
 
         if (currentHealth<=0)
         {
