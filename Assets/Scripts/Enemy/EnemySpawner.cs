d5e2fa0719a2d2b08cc9ed11be2e2f7e8aa575ff
drhoneyroll <orlicaleksandar97@gmail.com>
2026-03-30 21:23:42 +0200

    EnemySpawn

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+
+    [SerializeField] private float _minimumSpawnTime;
+
+    [SerializeField] private float _maximumSpawnTime;
+
+    [SerializeField] private float _timeUntilSpawn;
+
+    private float timer = 0;
+
+    void Awake()
+    {
+        SetTimeUntilSpawn();
+    }
+
+    void Start()
+    {
+        StartCoroutine(CountEvery15Seconds());
+    }
+
+    private IEnumerator CountEvery15Seconds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(15f);
+            _minimumSpawnTime+=5;
+            _maximumSpawnTime-=5;
+            //Debug.Log("15 seconds have passed");
+        }
+    }
+
+    void Update()
+    {
+        _timeUntilSpawn -=  Time.deltaTime;
+        timer+=Time.deltaTime;
+        //Debug.Log("Time "+timer+" moduo: "+(timer % 15));
+        /*
+        if (timer % 15 <= 0.001)
+        {
+            Debug.Log("Proslo 15 sekundi: " + timer);
+            _minimumSpawnTime+=5;
+            _maximumSpawnTime-=5;
+        }
+        */
+        if (_timeUntilSpawn <= 0)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
+    private void SetTimeUntilSpawn()
+    {
+        _timeUntilSpawn=Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+        _timeUntilSpawn=Mathf.Clamp(_timeUntilSpawn, 1f, 2f);
+    }
+}
